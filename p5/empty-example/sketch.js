a11622bf612ba949147549a2a7ccce6dc6a3cbc5
clayd <clay.davis@gmail.com>
2019-07-14 19:24:08 -0400

    random color lines on canvas

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -8,11 +8,30 @@ function setup() {
 }
 
 function draw() {
-  x1 = Math.floor(Math.random() * 1024);
-  y1 = Math.floor(Math.random() * 768);
-  x2 = Math.floor(Math.random() * 1024);
-  y2 = Math.floor(Math.random() * 768);
+  let x1 = Math.floor(Math.random() * 1024);
+  let y1 = Math.floor(Math.random() * 768);
+  let x2 = Math.floor(Math.random() * 1024);
+  let y2 = Math.floor(Math.random() * 768);
 
-   line(x1,y1,x2,y2);
-   
+  //stroke(getRandomHexColor());
+  let b = Math.floor(Math.random() * 255);
+  let r = Math.floor(Math.random() * 255);
+  let g = Math.floor(Math.random() * 255);
+
+  let a = Math.random();
+
+  let sRgba = 'rgba('+ r +','+ g +','+ b +',.3)'
+  stroke(sRgba);
+
+  line(x1,y1,x2,y2);
+
+}
+
+function getRandomHexColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
